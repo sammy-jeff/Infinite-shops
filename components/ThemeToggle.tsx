7407316ab47452b831186b8dc2211362944cade2
sammy-jeff <okoyechibuike048@gmail.com>
2023-10-07 11:49:56 +0100

    made some minor changes

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,13 +1,13 @@
 'use client'
-import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faLightbulb, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import {useTheme} from 'next-themes'
 const ThemeToggle = () => {
     const {resolvedTheme,setTheme} = useTheme()
   return (
-    <div className='fixed bottom-7 right-2 z-10 p-2 rounded-full h-fit w-fit bg-[rgba(0,0,0,0.3)]' onClick={()=>setTheme(resolvedTheme==='dark'?'light':'dark')}>
-        <FontAwesomeIcon icon={faMoon} width={30} height={30} className={`text-[clamp(16px,4vw,30px)]`}/>
+    <div className='fixed bottom-7 right-2 z-10 p-2 rounded-full h-fit w-fit bg-[rgba(0,0,0,0.3)] cursor-pointer' onClick={()=>setTheme(resolvedTheme==='dark'?'light':'dark')}>
+        <FontAwesomeIcon icon={resolvedTheme==='dark'?faLightbulb:faMoon} width={30} height={30} className={`text-[clamp(16px,4vw,30px)]`}/>
     </div>
   )
 }
